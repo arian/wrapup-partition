09eb3d004f3180de8470f6f0c1ec71900e4a299b
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-11-06 02:48:21 +0100

    Add option to compress module names

--- bin/cli.js
+++ bin/cli.js
@@ -12,6 +12,7 @@ program.command('partition')
     .option('--path <path>', 'The base path of the modules, so <path>/bar/foo.js becomes bar/foo as module ID')
     .option('--map <file>', 'A JSON file containing the mapping of the modules')
     .option('--ast', 'Output the Abstract Syntax Tree as JSON')
+    .option('--compress', 'Compress module names')
     .action(function(args){
 
         if (!args.output){
@@ -28,6 +29,7 @@ program.command('partition')
         partition.set('output', args.output)
         partition.set('path', args.path)
         partition.set('ast', args.ast)
+        partition.set('compress', args.compress)
 
         if (args.map){
             var map = JSON.parse(fs.readFileSync(args.map))
--- includes/main.js
+++ includes/main.js
@@ -1,12 +1,23 @@
 (function(){
     var map = {files: [], modules: {}}
     var modules = map.modules, files = map.files
-    var paths = {}
+    var paths = {}, defines = {}
     for (var name in modules){
-        paths[name] = files[modules[name]].slice(0, -3)
+        paths[true ? modules[name][1] : name] = files[modules[name][0]].slice(0, -3)
+        defines[name] = true ? modules[name][1] : name
     }
     window.global = window
     requirejs.config({
         paths: paths
+    })
+    define('wrapup-names', defines)
+    define('wrapup-paths', paths)
+    define('wrapup-require', function(){
+        return function(ids, success, error){
+            for (var i = 0; i < ids.length; i++) {
+                ids[i] = defines[ids[i]]
+            }
+            requirejs(ids, success, error)
+        }
     })
 })()
